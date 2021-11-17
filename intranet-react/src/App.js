b827c94ed44dd550a46d20c59e7758cd5197cfed
jonsinmar <sinde.jon@gmail.com>
2021-11-17 17:47:02 +0100

    guardar estado login en localStorage

--- intranet-react/src/App.js
+++ intranet-react/src/App.js
@@ -3,9 +3,10 @@ import "./App.css";
 import Login from "./login";
 import { Routes, Route } from "react-router-dom";
 import Directorio from "./directorio";
+import useAuth from "./hooks/useAuth";
 
 function App() {
-  const [authState, setAuthState] = useState(false);
+  const { authState, setAuthState } = useAuth();
   /**
    * Usamos Routes para agrupar las rutas que tendrá nuestra aplicación. Cuando haya un cambio en la ruta del navegador, si coincide con alguna de las rutas, se ejecutará el componente correspondiente.
    */
