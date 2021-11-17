ceb03d447560fdbe315c84f597112ecf1905026d
jonsinmar <sinde.jon@gmail.com>
2021-11-17 17:38:59 +0100

    navegación "post-login"

--- intranet-react/src/App.js
+++ intranet-react/src/App.js
@@ -1,17 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 import Login from "./login";
 import { Routes, Route } from "react-router-dom";
 import Directorio from "./directorio";
 
 function App() {
-  return (
-    /**
-     * Usamos Routes para agrupar las rutas que tendrá nuestra aplicación. Cuando haya un cambio en la ruta del navegador, si coincide con alguna de las rutas, se ejecutará el componente correspondiente.
-     */
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/directorio" element={<Directorio />} />
-    </Routes>
-  );
+  const [authState, setAuthState] = useState(false);
+  /**
+   * Usamos Routes para agrupar las rutas que tendrá nuestra aplicación. Cuando haya un cambio en la ruta del navegador, si coincide con alguna de las rutas, se ejecutará el componente correspondiente.
+   */
+
+  if (!authState) {
+    return (
+      <Routes>
+        <Route path="/" element={<Login setAuthState={setAuthState} />} />
+      </Routes>
+    );
+  } else {
+    return (
+      <Routes>
+        <Route path="/" element={<Directorio />} />
+      </Routes>
+    );
+  }
 }
+
 export default App;
