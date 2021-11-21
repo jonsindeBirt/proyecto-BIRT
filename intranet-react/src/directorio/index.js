4222927e3bf6af2e4c70b18e6901c107a3becb2b
sulanak <77105025+susanabirt@users.noreply.github.com>
2021-11-22 00:29:48 +0100

    directorio-uno
    comienzo desplegable

--- intranet-react/src/directorio/index.js
+++ intranet-react/src/directorio/index.js
@@ -1,18 +1,18 @@
 import fondodirectorio from "../assets/fondo.jpg";
 //importamos el compoente Link desde react-router-dom
 import { Link } from "react-router-dom";
+import React from "react";
+import Desple from "./components/desplegable";
+
 
 function Directorio() {
   return (
-    <div>
-      {/* usamos el componente Link como si fuera un <a> de HTML. En el 'to' ponemos la ruta a donde queremos que nos lleve */}
-      <Link to="calendario">Calendario</Link>
-      <img
-        className="imagenfondodirectorio"
-        src={fondodirectorio}
-        alt="fondodirectorio"
-      ></img>
-    </div>
+  
+      <div className="row">
+          {/* usamos el componente Link como si fuera un <a> de HTML. En el 'to' ponemos la ruta a donde queremos que nos lleve */}
+        <Desple/>
+
+      </div>
   );
 }
 
